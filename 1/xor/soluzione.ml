30bdf6beb5718e0c29cf1ba65af09c2d752f7f2b
mikiichillo <chillottim@gmail.com>
2024-10-11 12:43:46 +0200

    xor

--- 1/xor/soluzione.ml
+++ 1/xor/soluzione.ml
@@ -1,6 +1,14 @@
-(*let xor1 (a:bool) (b:bool) : bool = (a||b) and not(a&&b)*)
+let xor1 (a:bool) (b:bool) : bool = (a && not b) || (not a && b)
 
-let xor2 (a:bool) (b:bool) : bool = if a then (if b then false else true) else if b then true else false
+let xor2 (a:bool) (b:bool) : bool = 
+  if a 
+    then 
+      (if b then false 
+      else true) 
+    else 
+      if b 
+        then true 
+  else false
 
 let xor3 (a:bool) (b:bool) : bool = 
     match (a,b) with
