f6b5d1e230ccde835e98f46d879d4b9d36169379
mikiichillo <chillottim@gmail.com>
2024-10-11 12:24:44 +0200

    xor

--- 1/xor/soluzione.ml
+++ 1/xor/soluzione.ml
@@ -1,3 +1,9 @@
-let b = true
+(*let xor1 (a:bool) (b:bool) : bool = (a||b) and not(a&&b)*)
 
-let conj b1 b2 = b1 || b2
+let xor2 (a:bool) (b:bool) : bool = if a then (if b then false else true) else if b then true else false
+
+let xor3 (a:bool) (b:bool) : bool = 
+    match (a,b) with
+    | (true, true) -> false
+    | (false, false) -> false
+    | _ -> true
