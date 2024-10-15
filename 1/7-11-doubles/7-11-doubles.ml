b2eb1ab7cb3d2103805979cb12099a26f69012f9
mikiichillo <chillottim@gmail.com>
2024-10-15 13:32:47 +0200

    exercises

---
+++ 1/7-11-doubles/7-11-doubles.ml
@@ -0,0 +1,4 @@
+let fun1 (a : int) (b : int) : bool = 
+  if (a+b == 7 || a+b == 11 || a == b)
+    then true
+else false 
---
+++ 1/absolute-value/absolute-value.ml
@@ -0,0 +1,4 @@
+let fun1 (x : int) : int =
+    if x < 0 
+        then 0 - x 
+    else x
